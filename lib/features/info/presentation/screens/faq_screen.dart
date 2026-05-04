91d5b635573158162a387c71669067e437efc51e
Hemen Solomon <hemensolomon83@gmail.com>
2026-05-04 22:48:47 +0300

    finalize placeholder screens

---
+++ lib/features/info/presentation/screens/faq_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FAQScreen extends StatelessWidget {
+  const FAQScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('FAQ Screen')),
+    );
+  }
+}
