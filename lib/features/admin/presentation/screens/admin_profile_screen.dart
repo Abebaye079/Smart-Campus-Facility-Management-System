ada75887da026d8a1ee2a6cd52b85a896147a1c3
Nanat21 <nanatabeshu97@gmail.com>
2026-05-07 21:39:19 +0300

    Refine admin profile UI and improve layout consistency

--- lib/features/admin/presentation/screens/admin_profile_screen.dart
+++ lib/features/admin/presentation/screens/admin_profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+
 import '../../../../core/theme/app_colors.dart';
 import '../../../../core/constants/route_names.dart';
 import '../../../../core/widgets/main_header.dart';
